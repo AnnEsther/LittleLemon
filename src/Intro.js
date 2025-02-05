087ffc8a9af81f2a62bee116ad3d2fee9a48c90c
Ann Joy <annjoymathew@gmail.com>
2025-02-04 19:08:19 -0800

    Adding CSS changes for smaller screen

--- src/Intro.js
+++ src/Intro.js
@@ -4,10 +4,13 @@ export default function Intro() {
     return (
         <div id="home" className="intro">
             <div className="introContent">
-                <h1>Little Lemon</h1>
-                <h2>Chicago</h2>
-                <h6>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</h6>
-                <button className='styledButton'> Reserve a table</button>
+                <h1 className="introHeading">Little Lemon</h1>
+                <h2 className="introSubHeading">Chicago</h2>
+                <h6 className="introPara">We are a family owned Mediterranean restaurant, 
+                    focused on traditional recipes served with a modern twist.</h6>
+                    <div className="introButtonDiv">
+                        <button className='styledButton'> Reserve a table</button>
+                    </div>
                 </div>
             <div className="introImage">
                 <img className="introImg" src={introBg} ></img>
