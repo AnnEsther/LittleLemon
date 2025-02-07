495cd5cd6f849921ccfe716f92238a6303bbc753
Ann Joy <annjoymathew@gmail.com>
2025-02-07 10:36:56 -0800

    Adding Routes

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,35 @@
-import Intro from "./Intro";
+import { Route, Routes } from "react-router-dom"; import Intro from "./Intro";
 import Specials from "./Specials";
 import Testimonial from "./Testimonial";
 import About from "./About";
 
+
 export default function Main() {
     return (
         <main>
-            <Intro/>
-            <Specials/>
-            <Testimonial/>
-            <About/>
+            <Routes>
+                    <Route path="/" element={<HomePage />}></Route>
+                    <Route path="/booking" element={<BookingPage />}></Route>
+            </Routes>
         </main>
+    );
+}
+
+function HomePage() {
+    return (
+        <>
+            <Intro />
+            <Specials />
+            <Testimonial />
+            <About />
+        </>
+    );
+}
+
+function BookingPage() {
+    return (
+        <>
+            <h1>Under Construction!</h1>
+        </>
     );
 }
