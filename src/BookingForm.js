b5af1d622039f76f6fcac6abe9bd6c25ad66b6c6
Ann Joy <annjoymathew@gmail.com>
2025-02-10 14:07:18 -0800

    Adding Booking Pade

---
+++ src/BookingForm.js
@@ -0,0 +1,62 @@
+
+import React, { useState } from "react";
+
+
+export default function BookingForm() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const [date, setDate] = useState("");
+    const handleDateChange = (e) => {
+        setDate(e.target.value)
+    }
+
+    const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]);
+    const [time, setTime] = useState(availableTimes[0]);
+    const handleTimeChange = (e) => {
+        setTime(e.target.value)
+    }
+
+    const [noGuests, setNoGuests] = useState("1");
+    const handleGuestsChange = (e) => {
+        setNoGuests(e.target.value)
+    }
+
+    const [occasion, setOccasions] = useState("None");
+    const handleOccasionChange = (e) => {
+        setOccasions(e.target.value)
+    }
+
+    return (
+        <>
+            <div className="bookingPage">
+                <h3>Reserve a Table</h3>
+                <form onSubmit={handleSubmit} className="bookingForm">
+                    <div className="bookingMainDiv">
+                    <label className="dateLabel" for="res-date">Choose date</label>
+                    <input className="dateInput" type="date" id="res-date" value={date} onChange={handleDateChange} />
+
+                    <label className="timeLabel" for="res-time">Choose time</label>
+                    <select className="timeInput" id="res-time " value={time} onChange={handleTimeChange}>
+                        {availableTimes.map(availableTime => { return <option>{availableTime}</option> })}
+                    </select>
+
+                    <label className="guestsLabel" for="guests">Number of guests</label>
+                    <input className="guestsInput" type="number" placeholder="1" min="1" max="10" id="guests" value={noGuests} onChange={handleGuestsChange} />
+
+                    <label className="occasionLabel" for="occasion">Occasion</label>
+                    <select className="occasionInput" id="occasion" value={occasion} onChange={handleOccasionChange}>
+                        <option>None</option>
+                        <option>Birthday</option>
+                        <option>Anniversary</option>
+                    </select>
+                    </div>
+                    <div>
+                        <input className="formSubmit" type="submit" value="Make Your reservation" />
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+}
--- src/Main.js
+++ src/Main.js
@@ -2,14 +2,15 @@ import { Route, Routes } from "react-router-dom"; import Intro from "./Intro";
 import Specials from "./Specials";
 import Testimonial from "./Testimonial";
 import About from "./About";
+import BookingForm from "./BookingForm";
 
 
 export default function Main() {
     return (
         <main>
             <Routes>
-                    <Route path="/" element={<HomePage />}></Route>
-                    <Route path="/booking" element={<BookingPage />}></Route>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path="/booking" element={<BookingPage />}></Route>
             </Routes>
         </main>
     );
@@ -27,9 +28,13 @@ function HomePage() {
 }
 
 function BookingPage() {
+
+
     return (
         <>
-            <h1>Under Construction!</h1>
+
+            <BookingForm></BookingForm>
+
         </>
     );
 }
